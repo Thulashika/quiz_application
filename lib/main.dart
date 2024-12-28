2d39268ce9ba91c89dc6502ac16d9a0760d7b6b5
Thulashika <lasibala24@gmail.com>
2024-12-28 14:48:13 +0530

    Called the start screen within the scaffold and added a container as it's parent and  added a linear gradient.

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/results_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors:[Colors.blue.shade900, Colors.blue.shade100])
+        ),
+        child: ResultsScreen()),
+    ))); 
+}
